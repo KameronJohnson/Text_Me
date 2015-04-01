c579e17973d76e1f35a1a4d95acd1b0a289250ef
Kameron <KameronJohnson@gmail.com>
2015-04-01 16:42:12 -0700

    add contact model

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root :to => 'messages#index'
-  
+
   resources :messages, :only => [:index, :new, :create, :show]
+  resources :contacts, :only => [:index, :new, :create, :show]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150401221104) do
+ActiveRecord::Schema.define(version: 20150401233528) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "phone"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "messages", force: :cascade do |t|
     t.string "from"
