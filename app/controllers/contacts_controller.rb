adec981680d11da955e93ea240f1d59408b4b982
Kameron <KameronJohnson@gmail.com>
2015-04-01 16:56:42 -0700

    add contacts controller

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,26 @@
+class ContactsController < ApplicationController
+
+  def index
+    @contacts = Contact.all
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      flash[:notice] = "Contact added"
+      redirect_to contacts_path
+    else
+      render :new
+    end
+  end
+
+private
+  def contact_params
+    params.require(:contact).permit(:name, :phone)
+  end
+
+end
