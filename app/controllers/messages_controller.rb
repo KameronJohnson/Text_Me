9045ff70bcf744fcbd320f4fdbe48d80656cd387
Kameron <KameronJohnson@gmail.com>
2015-04-02 13:50:30 -0700

    alter views, add destroy method to message controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -19,8 +19,18 @@ class MessagesController < ApplicationController
     end
   end
 
+  def edit
+    @message = Message.find(params[:id])
+  end
+
   def show
     @message = Message.find(params[:id])
+  end
+
+  def destroy
+    @message = Message.find(params[:id])
+    @message.destroy
+    redirect_to messages_path
   end
 
 
